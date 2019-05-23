c7fe4a8abe6780907aed0fca59b4efec1728f8aa
Praveen Shukla <praveen.cs06@gmail.com>
2019-05-23 20:03:16 +0530

    first commit

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using WebApplication1.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Web
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DemoContext>
+    {
+        public DemoContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DemoContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);       
+
+            return new DemoContext(builder.Options);
+        }
+    }
+}
---
+++ Domain/DemoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Domain
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options)
+             : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
